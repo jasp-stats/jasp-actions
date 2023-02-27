a9d17efd0e11652f522ea2c6dc122bca06ad2ba6
Shun Wang <shuonwang@gmail.com>
2023-02-27 16:45:50 +0800

    use jasp action branch

--- translations/i18nCheck.R
+++ translations/i18nCheck.R
@@ -1,25 +1,25 @@
-# For developers to detect unreasonable calls in code to translate strings
-# both applies to the R and QML code of the analysis module
+# For developers to detect unreasonable calls in code to translate strings.
+# both applies to the R and QML code of the analysis module.
 
 library(potools)
 library(cli)
 
 checkStatus <- c()
-# Generate pot meta data from R
+# Generate pot meta data from .R
 cli_h1("Check R translations:")
 rPotData <- potools::get_message_data(dir = ".",verbose = TRUE)
 
-# Get wrong usage of gettext of R
+# Get wrong usage of gettext from .R
 rErrorCalls <- subset(rPotData, rPotData$msgid=="", select = c("file", "call", "line_number"))
           
 if (nrow(rErrorCalls) > 0) {
   checkStatus <- c(checkStatus, 1)
   # Warning in CLI
-  cat(cli_alert_danger("{nrow(rErrorCalls)} empty gettext call(s) found"))
+  cli_alert_danger("{nrow(rErrorCalls)} empty gettext call(s) found")
   cli_h2("Please refer to following to resolve them:")
   print.data.frame(rErrorCalls, row.names = FALSE)
   }else{
-    cli_alert_success("R message check PASS")
+    cli_alert_success("R message check PASSED")
 } 
 
 cli_h1("Check QML translations:")
@@ -27,7 +27,7 @@ cli_h1("Check QML translations:")
 qmlFiles <- as.list(list.files(path = ".", pattern = ".qml", recursive = T))
 
 if (length(qmlFiles) == 0) {
-  cli_alert("QML files not found")
+  cli_alert("No QML file found")
   } else {
   # Generate pot meta data from QML
   qmlSrcData <- data.frame()
@@ -36,14 +36,13 @@ if (length(qmlFiles) == 0) {
   for (i in 1:length(qmlFiles)) {
     filePaths <- paste0("./", qmlFiles[[i]])
     readL     <- as.data.frame(readLines(filePaths, warn = FALSE))
-    
     tempData  <- data.frame(
       Source_file      = rep(filePaths), 
-      Call_code        = readLines(filePaths, warn = FALSE), 
+      Call_code        = readL[,1], 
       Line_number      = 1:nrow(readL),
       Translation_call = grepl(pattern="qsTr(|Id|anslate)\\(\".*\"\\)", readL[,1])+0,
-      Empty_call       = grepl(pattern="qsTr(|Id|anslate)\\(\"*\"\\)", readL[,1])+0
-    )
+      Empty_call       = grepl(pattern="qsTr(|Id|anslate)\\(\"*\"\\)", readL[,1])+0)
+
     qmlSrcData <- rbind(qmlSrcData, tempData)
   }
 
@@ -55,13 +54,13 @@ if (length(qmlFiles) == 0) {
     cli_h2("Please refer to following to resolve them:")
     print.data.frame(qmlErrorCalls, row.names = FALSE)
   } else {
-    cli_alert_success("QML message check PASS")
+    cli_alert_success("QML message check PASSED")
   }
 } 
 
 if (length(checkStatus) > 0){
-  # to fail github action
+  # failing github action,custom exit code to different from a system exit
   quit(status = 10)
 } else{
-  cli_alert_success("All i18n check PASS")
+  cli_alert_success("All i18n check PASSED")
 }
